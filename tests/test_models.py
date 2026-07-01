1f1becdda1e771681caeac19d10792ea9ce85f99
KavyashreeDalawai <kavyashri.dalawai@gmail.com>
2026-07-01 01:21:28 -0400

    updated test route and routes python again

--- tests/test_models.py
+++ tests/test_models.py
@@ -117,8 +117,8 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found_product.name, product.name)
         self.assertEqual(found_product.description, product.description)
         self.assertEqual(found_product.price, product.price)
-
-   def test_update_a_product(self):
+        
+    def test_update_a_product(self):
         """It should Update a Product"""
         product = ProductFactory()
         product.id = None
